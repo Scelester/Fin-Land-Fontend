3c9debd86b0e34db7cb26eece9e9436102cf7078
scelester <nabinpauudel664@gmail.com>
2022-11-25 19:36:11 +0545

    allthis

--- app/Finland/App.js
+++ app/Finland/App.js
@@ -1,15 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import {Button, StyleSheet, Text, View} from 'react-native';
 
+import { useWindowDimensions } from 'react-native';
+import RenderHtml from 'react-native-render-html';
+
+
+const homesource =	{
+	html:`
+	<p>This is a test</p>
+	`
+	}
 
 
 
 export default function App() {
+
+	const {width} = useWindowDimensions();	
   return (
     <View style={styles.container}>
       <Text>Sailesh</Text>
       <Button title={"This does nothing"} />
-      <StatusBar style="auto" />
+
+	  <RenderHtml contentWidth={width}
+					source={homesource}
+					/>
+
+	  <StatusBar style="auto" />
 
     </View>
   );
