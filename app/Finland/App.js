0c79d9259cce32ae0598f31fb62fe3619a2ed456
scelester <nabinpauudel664@gmail.com>
2022-11-30 13:16:37 +0545

    fixed

--- app/Finland/App.js
+++ app/Finland/App.js
@@ -1,36 +1,38 @@
+
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View,BackHandler } from 'react-native';
+import { Button, StyleSheet, Text, View, BackHandler } from 'react-native';
 
+import React from 'react';
 
-import { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { useFocusEffect } from '@react-navigation/native';
 import { WebView } from 'react-native-webview';
-// import RenderHtml from 'react-native-render-html';
 
 
 
 
 
 export default function App() {
+  const [pannel,setpannel] = React.useState();
 
-  useEffect(()=>{
-    BackHandler.addEventListener("hardwareBackPress",()=>{return false});
+  React.useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', () => { return true }) 
 
-    ()=>{BackHandler.removeEventListener("hardwareBackPress",true)};
   });
 
   return (
     <>
-    <View style={{ flex: 1 }} >
-      <StatusBar backgroundColor='#2A6472' />
-      <WebView
-      useWebView2={true}
-        source={{ uri: 'https://scelester.github.io/Fin-Land-Fontend/' }}
-        style={styles.container}
-        setBuiltInZoomControls={false}
-        overScrollMode='never'
-      />
-      <Stack.Screen name="Home" component={the} />
-    </View>
+      <View style={{ flex: 1 }} >
+        <StatusBar backgroundColor='#2A6472' />
+        <WebView
+          useWebView2={true}
+          source={{ uri: 'https://scelester.github.io/Fin-Land-Fontend/' }}
+          style={styles.container}
+          setBuiltInZoomControls={false}
+          overScrollMode='never'
+        />
+      </View>
+
     </>
   );
 }
@@ -38,7 +40,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop:30,
+    marginTop: 30,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
