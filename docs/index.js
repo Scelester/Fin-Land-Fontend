b690f037f383f95db0c4d2bcbb2ef5a3106230cc
scelester <nabinpauudel664@gmail.com>
2022-12-02 19:18:39 +0545

    dafsd

--- docs/index.js
+++ docs/index.js
@@ -1,14 +1,21 @@
+const supabaseurl = "https://eobfgehqjibbzwripnmd.supabase.co"
+const supabsekey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvYmZnZWhxamliYnp3cmlwbm1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk5NDkwNDIsImV4cCI6MTk4NTUyNTA0Mn0.w-siG2qQ0MdCkugJH_CLb_w4FwQXfMp81QXTxZJ4yQc"
+const supabase = createClient(supabaseurl, supabsekey)
 
 
-const Verify_user = () => {
-    const supabaseurl = process.env.SUPABASEURL
-    const supabsekey = process.env.SUPABASEKEY
+const Verify_user = async () => {
+    let { user: User_Authentication, error } = await supabase
+    .from('User_Authentication')
+
+    console.log("User:",user,"error:",error)
+
 };
+
 
 
 
 let login_button = document.getElementById("loginbutton");
 
 login_button.addEventListener("click", () => {
-    alert("me")
+    Verify_user();
 })
