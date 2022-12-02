bd39930cbffc40061504181e8261e289ebe1b830
scelester <nabinpauudel664@gmail.com>
2022-12-02 18:50:09 +0545

    dafsd

--- docs/index.js
+++ docs/index.js
@@ -1,18 +1,13 @@
 require('dotenv').config()
 
-
 const Verify_user = () => {
     const supabaseurl = process.env.SUPABASEURL
     const supabsekey = process.env.SUPABASEKEY
-
-    console.log(supabaseurl)
-    console.log("test")
-    alert("ok")
 };
 
 
 
-var login_button = document.getElementById("loginbutton");
+let login_button = document.getElementById("loginbutton");
 
-login_button.addEventListener("click",Verify_user());
+login_button.addEventListener("click",alert("ok"));
 
