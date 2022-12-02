201b32ef25075d78ec71195edf3f197974be7306
scelester <nabinpauudel664@gmail.com>
2022-12-02 19:37:45 +0545

    dafsd

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,4 @@
+
 const supabaseurl = "https://eobfgehqjibbzwripnmd.supabase.co"
 const supabsekey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvYmZnZWhxamliYnp3cmlwbm1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk5NDkwNDIsImV4cCI6MTk4NTUyNTA0Mn0.w-siG2qQ0MdCkugJH_CLb_w4FwQXfMp81QXTxZJ4yQc"
 const supabase = createClient(supabaseurl, supabsekey)
