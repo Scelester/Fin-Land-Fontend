8ee5915730d15b7b9162fa0c02b47ec3808c4308
scelester <nabinpauudel664@gmail.com>
2022-12-02 18:24:11 +0545

    dafsd

--- docs/index.js
+++ docs/index.js
@@ -1,1 +1,16 @@
-alert("test")
+
+
+const Verify_user = () => {
+    const supabaseurl = process.env.SUPABASEURL
+    const supabsekey = process.env.SUPABASEKEY
+
+    console.log(supabaseurl)
+
+};
+
+
+
+var login_button = document.getElementById("loginbutton");
+
+login_button.addEventListener("click",Verify_user);
+
