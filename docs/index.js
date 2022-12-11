9873ac261306c5bc582568b270b627de77eb1673
scelester <nabinpauudel664@gmail.com>
2022-12-11 08:38:28 +0545

    dfa

--- docs/index.js
+++ docs/index.js
@@ -1,23 +1,40 @@
 
-const supabaseurl = "https://eobfgehqjibbzwripnmd.supabase.co"
-const supabsekey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvYmZnZWhxamliYnp3cmlwbm1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk5NDkwNDIsImV4cCI6MTk4NTUyNTA0Mn0.w-siG2qQ0MdCkugJH_CLb_w4FwQXfMp81QXTxZJ4yQc"
-const supabase = createClient(supabaseurl, supabsekey)
+var SUPABASE_URL = 'https://eobfgehqjibbzwripnmd.supabase.co'
+var SUPABASE_KEY ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvYmZnZWhxamliYnp3cmlwbm1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk5NDkwNDIsImV4cCI6MTk4NTUyNTA0Mn0.w-siG2qQ0MdCkugJH_CLb_w4FwQXfMp81QXTxZJ4yQc'
 
 
-const Verify_user = async () => {
-    
-    let { user, error } = await supabase
-    .from('User_Authentication')
+var supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+window.userToken = null
 
-    alert(user)
+document.addEventListener('DOMContentLoaded', function (event) {
 
-};
+  var logInbutton = document.querySelector('#loginbutton')
+  logInbutton.addEventListener('click', () => {logInSubmitted()})
 
-
-
-
-let login_button = document.getElementById("loginbutton");
-
-login_button.addEventListener("click", () => {
-    Verify_user();
 })
+
+const logInSubmitted = (event) => {
+
+  const email = document.getElementById("username").value
+  const password = document.getElementById("password").value
+
+  supabase.auth
+    .signIn({ email, password })    
+    .then((response) => {
+      response.error ? alert(response.error.message) : setToken(response)
+    })
+    .catch((err) => {
+      alert(err.response.text)
+    })
+    
+}
+async function xxx(){
+const { data: user, error } = await supabase.auth.api.createUser({
+
+    email: 'user',
+  
+    password: 'password',
+  
+  })
+
+}
